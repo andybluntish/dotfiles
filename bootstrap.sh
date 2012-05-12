4f8a6fed57c692656840e7a9f04de6e9623a6c1e
Andy Bluntish <a.bluntish@gmail.com>
2012-05-13 00:56:47 +1000

    Replaced Rakefile with bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+###
+#
+# bootstrap.sh
+#
+# Setup and sync dotfiles
+#
+# 1. Copy files from the local repository into ~/.init
+# 2. Symlink files from ~/.init into the home directory
+#    Files starting with an underscore (_) are NOT symlinked
+# 3. Profit
+#
+###
+
+
+# Change into the directory we're executing from.
+cd `dirname $0`
+
+# Make the ~/.init directory if it doesn't exist.
+if [ ! -d "~/.init" ]; then
+  mkdir "~/.init"
+fi
+
+# Rsync files to ~/.init, excluding: git files, Readme, and this script.
+rsync --exclude `basename $0` --exclude ".DS_Store" --exclude ".git/" --exclude ".gitignore" --exclude "README.md" --archive --verbose --delete . ~/.init
+
+# Loop through each file in ~/.init
+for file in `ls ~/.init`; do
+
+  # Skip files that begin with an underscore
+  if [[ $file =~ ^_.* ]]; then
+    continue
+  fi
+
+  # Symlink file to the home directory
+  ln -snf ~/.init/$file ~/.$file
+done
+
+# Reload the bash_profile
+source ~/.bash_profile
+
