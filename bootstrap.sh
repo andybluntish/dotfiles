152ffaa943cd9be3a34f8954f54dbcc7cb5be922
Andy Bluntish <a.bluntish@gmail.com>
2012-12-02 12:15:38 +1100

    Rename dir to

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,8 +6,8 @@
 #
 # Setup and sync dotfiles
 #
-# 1. Copy files from the local repository into ~/.init
-# 2. Symlink files from ~/.init into the home directory
+# 1. Copy files from the local repository into ~/.dotfiles
+# 2. Symlink files from ~/.dotfiles into the home directory
 #    Files starting with an underscore (_) are NOT symlinked
 # 3. Profit
 #
@@ -17,16 +17,16 @@
 # Change into the directory we're executing from.
 cd `dirname $0`
 
-# Make the ~/.init directory if it doesn't exist.
-if [ ! -d "~/.init" ]; then
-  mkdir "~/.init"
+# Make the ~/.dotfiles directory if it doesn't exist.
+if [ ! -d "~/.dotfiles" ]; then
+  mkdir "~/.dotfiles"
 fi
 
-# Rsync files to ~/.init, excluding: hidden files, Readme, and this script.
-rsync --exclude `basename $0` --exclude ".*" --exclude "README.md" --archive --verbose --delete . ~/.init
+# Rsync files to ~/.dotfiles, excluding: hidden files, Readme, and this script.
+rsync --exclude `basename $0` --exclude ".*" --exclude "README.md" --archive --verbose --delete . ~/.dotfiles
 
-# Loop through each file in ~/.init
-for file in `ls ~/.init`; do
+# Loop through each file in ~/.dotfiles
+for file in `ls ~/.dotfiles`; do
 
   # Skip files that begin with an underscore
   if [[ $file =~ ^_.* ]] || [[ $file =~ \.old$ ]]; then
@@ -34,7 +34,7 @@ for file in `ls ~/.init`; do
   fi
 
   # Symlink file to the home directory
-  ln -snf ~/.init/$file ~/.$file
+  ln -snf ~/.dotfiles/$file ~/.$file
 done
 
 # Reload the bash_profile
