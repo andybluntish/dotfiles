2ea95f93eeee52568c004d62181c750809610e9b
Andy Stanford-Bluntish <andy.stanford-bluntish@blake.com.au>
2017-07-19 12:12:00 +1000

    Bootstrap vim directry links
    Link directories under _vim/ to ~/.vim/
    
    Add custom Tabularize commands after plugin has loaded.

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,18 +8,34 @@
 #
 ###
 
-
-# Change into the directory we're executing from.
-cd `dirname $0`
-
 # Loop through each file
 for file in `ls .`; do
+  IN="$PWD/$file"
+  OUT="$HOME/.$file"
 
   # Skip files that begin with an underscore, readme, and the bootstrap file
   if [[ $file =~ ^_.* ]] || [[ $file =~ ^README ]] || [[ $file =~ ^bootstrap ]]; then
     continue
   fi
 
+  echo "Linking $IN -> $OUT"
+
   # Symlink file to the home directory
-  ln -snf "$PWD/$file" "$HOME/.$file"
+  ln -snf $IN $OUT
+done
+
+# Loop through vim files
+for file in `ls ./_vim`; do
+  IN="$PWD/_vim/$file"
+  OUT="$HOME/.vim/$file"
+
+  # Skip files that begin with an underscore, readme, and the bootstrap file
+  if [[ $file =~ ^_.* ]] || [[ $file =~ ^README ]] || [[ $file =~ ^bootstrap ]]; then
+    continue
+  fi
+
+  echo "VIM: Linking $IN -> $OUT"
+
+  # Symlink file to the home directory
+  ln -snf $IN $OUT
 done
