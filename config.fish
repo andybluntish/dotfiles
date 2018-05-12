5166ada9c6695a433b6b5747a241861ff7c08363
Andy Stanford-Bluntish <andy@bluntish.net>
2018-05-12 16:43:33 +1000

    not using fisherman

--- config.fish
+++ config.fish
@@ -1,7 +1,6 @@
 set fish_greeting
 
 set -x EDITOR nvim
-set -x FZF_LEGACY_KEYBINDINGS 0
 set -x FZF_DEFAULT_COMMAND 'git ls-tree -r --name-only HEAD 2> /dev/null; or fd --type f --hidden --follow --exclude .git 2> /dev/null'
 set -x FZF_CTRL_T_COMMAND $FZF_DEFAULT_COMMAND
 set -x GPG_TTY (tty)
