0dc076c61173f6a1d026817f55baf6cc9eebc06b
Andy Stanford-Bluntish <andy@bluntish.net>
2018-05-12 21:30:52 +1000

    don't set LS_COLORS

--- config.fish
+++ config.fish
@@ -5,6 +5,5 @@ set -x FZF_DEFAULT_COMMAND 'git ls-tree -r --name-only HEAD 2> /dev/null; or fd 
 set -x FZF_CTRL_T_COMMAND $FZF_DEFAULT_COMMAND
 set -x GPG_TTY (tty)
 set -x GREP_COLOR "30;43"
-set -x LS_COLORS 'di=34:fi=0:ln=35:or=31:mi=0:ex=35;1'
 
 set PATH $HOME/.bin $HOME/.yarn/bin $PATH
