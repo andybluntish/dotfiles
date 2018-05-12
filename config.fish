188e81ba75577c1d059d6e5a642c375cceb90c99
Andy Stanford-Bluntish <andy@bluntish.net>
2018-05-12 23:32:49 +1000

    setup alias functions

--- config.fish
+++ config.fish
@@ -7,3 +7,51 @@ set -x GPG_TTY (tty)
 set -x GREP_COLOR "30;43"
 
 set PATH $HOME/.bin $HOME/.yarn/bin $PATH
+
+function g; git $argv; end
+function gup; git gup; end
+function tree; command tree -aFCN -L 1 -I 'node_modules|bower_components|tmp|vendor|typings|.git' $argv; end
+function df; command df -h $argv; end
+function tmux; command tmux -2 $argv; end
+
+# Fuzzy find & vim
+function vp
+  if test (count $argv) -gt 0
+    eval $EDITOR $argv
+  else
+    fzf -m | xargs $EDITOR
+  end
+end
+
+# View files/dirs
+# - if directory, list the contents with tree
+# - if file(s), print the highlighted contents with pygmentize
+function c
+  if test (count $argv) -eq 0
+    tree ./
+    return
+  end
+
+  for i in $argv
+    set_color yellow
+    if test (count $argv) -gt 1; echo "$i:" 1>&2; end
+    set_color normal
+
+    if test -e $i; and test -r $i
+      if test -d $i
+        tree $i
+      # else if file -b --mime-type $i | string match -q -r '^image\/'
+      #   imgcat $i
+      else
+        pygmentize -O style=monokai -f console256 -g $i
+      end
+    else
+      set_color red
+      echo "Cannot open: $i" 1>&2
+    end
+
+    set_color normal
+  end
+end
+
+source ~/.asdf/asdf.fish
