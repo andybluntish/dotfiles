b7b1c4e8e4720273d528975bee0a618beaedab64
Andy Stanford-Bluntish <andy@bluntish.net>
2018-05-12 15:09:47 +1000

    initial fish config

---
+++ config.fish
@@ -0,0 +1,10 @@
+set fish_greeting
+
+set -x EDITOR nvim
+set -x FZF_LEGACY_KEYBINDINGS 0
+set -x FZF_DEFAULT_COMMAND 'git ls-tree -r --name-only HEAD 2> /dev/null; or fd --type f --hidden --follow --exclude .git 2> /dev/null'
+set -x FZF_CTRL_T_COMMAND $FZF_DEFAULT_COMMAND
+set -x GPG_TTY (tty)
+set -x GREP_COLOR "30;43"
+
+set PATH $HOME/.bin $HOME/.yarn/bin $PATH
