9b6aafc7b5f37712608a33ba1ae171291ed001c7
Andy Stanford-Bluntish <andy@bluntish.net>
2018-05-18 21:41:00 +1000

    define $VISUAL editor

--- config.fish
+++ config.fish
@@ -1,6 +1,7 @@
 set fish_greeting
 
 set -x EDITOR nvim
+set -x VISUAL nvim
 set -x FZF_DEFAULT_COMMAND 'git ls-tree -r --name-only HEAD 2> /dev/null; or fd --type f --hidden --follow --exclude .git 2> /dev/null'
 set -x FZF_CTRL_T_COMMAND $FZF_DEFAULT_COMMAND
 set -x GPG_TTY (tty)
