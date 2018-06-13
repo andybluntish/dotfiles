fb8b3d0e9728c61191fdf389da11ed28ff56da90
Andy Stanford-Bluntish <andy@bluntish.net>
2018-06-13 11:12:04 +1000

    add Ember server/test abbreviations

--- config/fish/functions/initialize_abbrs.fish
+++ config/fish/functions/initialize_abbrs.fish
@@ -1,4 +1,11 @@
 function initialize_abbrs
   abbr g 'git'
   abbr gup 'git gup'
+
+  abbr emp 'ember s --proxy https://api.flood.io'
+  abbr emm 'env MIRAGE=true ember s'
+
+  abbr etu 'ember test --filter "Unit | "'
+  abbr eti 'ember test --filter "Integration | "'
+  abbr eta 'ember test --filter "Acceptance | "'
 end
