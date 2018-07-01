d104b289372bc6935f0b130044cf5f74dd648f79
Andy Stanford-Bluntish <andy@bluntish.net>
2018-07-02 09:20:49 +1000

    more abbreviations

--- config/fish/functions/initialize_abbrs.fish
+++ config/fish/functions/initialize_abbrs.fish
@@ -1,5 +1,6 @@
 function initialize_abbrs
   abbr lf 'functions | tr ", " "\n" | fzf'
+  abbr fl 'cd ~/Code'
   abbr dsh 'cd ~/Code/dashboard'
   abbr nv 'node --version'
 
@@ -7,9 +8,12 @@ function initialize_abbrs
   abbr gup 'git gup'
 
   abbr emp 'ember s --proxy https://api.flood.io'
+  abbr eml 'ember s --proxy http://localhost:3000'
   abbr emm 'env MIRAGE=true ember s'
 
   abbr etu 'ember test --filter "Unit | "'
   abbr eti 'ember test --filter "Integration | "'
   abbr eta 'ember test --filter "Acceptance | "'
+
+  abbr be 'bundle exec'
 end
