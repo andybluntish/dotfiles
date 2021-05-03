68df8326efa97b99e18ac082a23dbf2225db9098
Andy Stanford-Bluntish <andy@bluntish.net>
2021-05-03 13:07:09 +0930

    configure Starship to show the current shell
    set the indicator string to blank for the default shell (fish)

--- bootstrap.sh
+++ bootstrap.sh
@@ -25,6 +25,7 @@ mkdir -p $HOME/.config/nvim
 link config/fish/config.fish $HOME/.config/fish/
 link config/nvim/init.vim $HOME/.config/nvim/
 link config/nvim/coc-settings.json $HOME/.config/nvim/
+link config/starship.toml $HOME/.config/
 
 link bash_profile
 link bashrc
