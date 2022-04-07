0782e99d09f72266ce38097694975d5c15d9c020
Andy Stanford-Bluntish <andy@bluntish.net>
2022-04-07 21:28:22 +0930

    cleanup bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,45 +3,24 @@
 set -euo pipefail
 [[ ${DEBUG:-} ]] && set -x
 
-function setup_grml() {
-  curl -L https://git.grml.org/f/grml-etc-core/etc/zsh/zshrc -o "${HOME}/.zshrc"
-}
+# Shell config from grml (https://grml.org/zsh/)
+curl -fLo "${HOME}/.zshrc" https://git.grml.org/f/grml-etc-core/etc/zsh/zshrc
 
-function link() {
-  source=$PWD/$1
-  target=${2:-$HOME/.$1}
+# Link dotfiles
+ln -snf ./zshrc.local ~/.zshrc.local
+ln -snf ./zshrc.custom.local ~/.zshrc.custom.local
+ln -snf ./bin ~/.bin
+ln -snf ./gemrc ~/.gemrc
+ln -snf ./gitconfig ~/.gitconfig
+ln -snf ./gitignore ~/.gitignore
+ln -snf ./hushlogin ~/.hushlogin
+ln -snf ./irbrc ~/.irbrc
 
-  echo "Linking $source -> $target"
+# NeoVim config
+mkdir -p "${HOME}/.config/nvim"
+link config/nvim/init.vim "${HOME}/.config/nvim/"
+link config/nvim/coc-settings.json "${HOME}/.config/nvim/"
 
-  ln -snf $source $target
-}
-
-function install_vim_plug() {
-  sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-
-  nvim +PlugUpgrade +PlugInstall +PlugUpdate +PlugClean! +qa
-}
-
-mkdir -p $HOME/.config/nvim
-
-# Config
-link config/nvim/init.vim $HOME/.config/nvim/
-link config/nvim/coc-settings.json $HOME/.config/nvim/
-link config/starship.toml $HOME/.config/
-
-# Shell
-link zshrc.local
-link zshrc.custom.local
-setup_grml
-
-# Misc
-link bin
-link gemrc
-link gitconfig
-link gitignore
-link hushlogin
-link inputrc
-link irbrc
-
-install_vim_plug
+# Install Vim Plug
+curl -fLo "${HOME}/.local/share/nvim/site/autoload/plug.vim" --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+nvim +PlugUpgrade +PlugInstall +PlugUpdate +PlugClean! +qa
