f6481ee9f323cf2ff4e97211eee6656f97cbae37
Andy Stanford-Bluntish <andy@bluntish.net>
2022-04-07 21:46:21 +0930

    fix links

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,20 +7,21 @@ set -euo pipefail
 curl -fLo "${HOME}/.zshrc" https://git.grml.org/f/grml-etc-core/etc/zsh/zshrc
 
 # Link dotfiles
-ln -snf ./zshrc.local ~/.zshrc.local
-ln -snf ./zshrc.custom.local ~/.zshrc.custom.local
-ln -snf ./bin ~/.bin
-ln -snf ./gemrc ~/.gemrc
-ln -snf ./gitconfig ~/.gitconfig
-ln -snf ./gitignore ~/.gitignore
-ln -snf ./hushlogin ~/.hushlogin
-ln -snf ./irbrc ~/.irbrc
+ln -snf "${PWD}/zshrc.local" "${HOME}/.zshrc.local"
+ln -snf "${PWD}/zshrc.custom.local" "${HOME}/.zshrc.custom.local"
+ln -snf "${PWD}/bin" "${HOME}/.bin"
+ln -snf "${PWD}/gemrc" "${HOME}/.gemrc"
+ln -snf "${PWD}/gitconfig" "${HOME}/.gitconfig"
+ln -snf "${PWD}/gitignore" "${HOME}/.gitignore"
+ln -snf "${PWD}/hushlogin" "${HOME}/.hushlogin"
+ln -snf "${PWD}/irbrc" "${HOME}/.irbrc"
 
 # NeoVim config
 mkdir -p "${HOME}/.config/nvim"
-link config/nvim/init.vim "${HOME}/.config/nvim/"
-link config/nvim/coc-settings.json "${HOME}/.config/nvim/"
+ln -snf "${PWD}/config/nvim/init.vim" "${HOME}/.config/nvim/"
+ln -snf "${PWD}/config/nvim/coc-settings.json" "${HOME}/.config/nvim/"
 
 # Install Vim Plug
 curl -fLo "${HOME}/.local/share/nvim/site/autoload/plug.vim" --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 nvim +PlugUpgrade +PlugInstall +PlugUpdate +PlugClean! +qa
