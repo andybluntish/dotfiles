679889c8437bbc7583ca45f679f24fac9ede499e
Andy Stanford-Bluntish <andy@bluntish.net>
2022-07-15 13:10:18 +0930

    quote vars in bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -21,26 +21,26 @@ link "hushlogin"
 link "irbrc"
 
 # Node.js
-node_version=16.15.1
+node_version="16.15.1"
 asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
-asdf install nodejs $node_version
-asdf global nodejs $node_version
+asdf install nodejs "${node_version}"
+asdf global nodejs "${node_version}"
 yarn global add neovim eslint prettier typescript
 asdf reshim nodejs
 
 # Ruby
-ruby_version=3.1.2
-asdf plugin add ruby https://github.com/asdf-vm/asdf-ruby.git
-asdf install ruby $ruby_version
-asdf global ruby $ruby_version
+ruby_version="3.1.2"
+asdf plugin add ruby "https://github.com/asdf-vm/asdf-ruby.git"
+asdf install ruby "${ruby_version}"
+asdf global ruby "${ruby_version}"
 gem install neovim bundler
 asdf reshim ruby
 
 # Python
-python_version=3.10.5
+python_version="3.10.5"
 asdf plugin-add python https://github.com/danhper/asdf-python
-asdf install python $python_version
-asdf global python $python_version
+asdf install python "${python_version}"
+asdf global python "${python_version}"
 pip install pynvim
 asdf reshim python
 
@@ -57,10 +57,10 @@ nvim +PlugUpgrade +PlugInstall +PlugUpdate +PlugClean! +qa
 $(brew --prefix)/opt/fzf/install --key-bindings --completion --no-update-rc --no-bash --no-fish
 
 function link() {
-  source=$PWD/$1
-  target=${2:-$HOME/.$1}
+  source="${PWD}/${1}"
+  target=${2:-"${HOME}/.${1}"}
 
-  echo "Linking $source -> $target"
+  echo "Linking ${source} -> ${target}"
 
-  ln -snf $source $target
+  ln -snf "${source}" "${target}"
 }
